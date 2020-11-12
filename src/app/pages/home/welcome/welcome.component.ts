7157480af6d59d8aca5aa57eb10b73b2483760ae
zhuweidong <2119523232@>
2020-11-12 11:15:18 +0800

    修改菜单

--- src/app/pages/home/welcome/welcome.component.ts
+++ src/app/pages/home/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 // import content from 'npm-dom-zhu';
 // import * as  content from 'npm-dom-zhu';
 
@@ -20,6 +22,57 @@ export class WelcomeComponent implements OnInit {
     const dd = require('npm-dom-zhu');
     dd.lai();
     dd.add(1, 5);
+
+    this.studyRxjx();
+    this.init();
   }
+
+  // Rxjs demo
+  studyRxjx() {
+    const observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 3000);
+    });
+
+    observable.subscribe({
+      next(res) { console.log('got value ' + res); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('done'); }
+    });
+  }
+
+
+  init() {
+    // map(
+    //   (x) => x * x)(of(1, 2, 3)).subscribe(
+    //     (v) => {
+    //       console.log(`value: ${v}`);
+    //     }
+    // );
+
+
+    const observable1 = interval(400);
+    const observable2 = interval(300);
+
+    const subscription = observable1.subscribe(x => console.log('first: ' + x));
+    const childSubscription = observable2.subscribe(x => console.log('second: ' + x));
+
+    subscription.add(childSubscription);
+
+    setTimeout(() => {
+      // Unsubscribes BOTH subscription and childSubscription
+      subscription.unsubscribe();
+    }, 1000);
+
+  }
+
+
+
+
 
 }
